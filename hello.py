31781cb88f80bde6bfe0a82baa360afdb3d75832
maxencegrados <maxence.grados@gmail.com>
2024-01-31 12:16:27 +0100

    ajout condition de fin de programme

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
-var = int(input("Entrez un nombre ? "))
-print(var)
-for i in range(1,11):
-    temp = var*i
-    print(temp)
+stop = False
+while stop != True :
+    var = int(input("Entrez un nombre ? :"))
+    print(var)
+    for i in range(1,11):
+        temp = var*i
+        print(temp)
+    a = input("voulez vous continuez ? oui ou non ? :")
+    if a == "non" :
+        stop = True
